766e96694fce9532b74580951d18b5027bdf8837
root <root@WIN-ICUK6SGIFLG>
2023-10-11 03:14:51 +0900

    asdf

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
 import gdb
 import struct
 
-addr = 0x602000 
-length = 0x50 
-bp = 0x400e9c 
-file_name = './secretgarden' 
-gdb.execute("source /home/msh/gef/gef.py")
-gdb.execute("file %s"%file_name)
-gdb.execute("b * %s"%hex(bp))
-gdb.execute("r")
+print('addr hex: ',end='')
+addr = int(input(),16)
+print('len hex: ',end='')
+length = int(input(),16)
 
 inf = gdb.inferiors()[0]
 m = inf.read_memory(addr,length).tolist()
@@ -22,6 +18,7 @@ for i in range(8):
         val = struct.unpack('<Q',(val))[0]
         sz = (val & 0x00000000ffffffff)
         if(0x20 <=sz <= 0x80):
-            print("found sz : ",hex(sz))
+            print("found sz : ",hex(sz&(~0b111)))
+            print('malloc sz : '+hex((sz>>4<<4)-0x10))
             print("addr : "+hex(addr+cur-0x8))
         cur += 8
